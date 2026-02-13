a4cb789030fcaeba65a7565ed93a50140c7155cc
Michael Marsh <mike@marsh.pw>
2026-02-13 11:08:03 -0500

    first commit, TDD for custom serdes

---
+++ LanguageExt.Json/Json.cs
@@ -0,0 +1,88 @@
+﻿using System.Text.Json;
+using LanguageExt.Common;
+using LanguageExt.Traits;
+using static LanguageExt.Prelude;
+
+namespace LanguageExt;
+
+public static class Json<F> where F : Fallible<F>, Applicative<F>
+{
+    private static K<F, A> @try<A>(Func<A> run) => Try.lift(run).Match(F.Pure, F.Fail<A>);
+
+    public static K<F, JsonElement> parse(Stream stream) 
+        => @try(() => JsonDocument.Parse(stream).RootElement)
+            .Catch(err => new JsonError("Could not parse json stream", err));
+
+    public static K<F, JsonElement> parse(string str) 
+        => @try(() => JsonDocument.Parse(str).RootElement)
+            .Catch(err => new JsonError($"Could not parse json result {limitLength(str)}", err));
+
+    public static K<F, Result> cast<Result>(JsonElement json)
+        => @try<Result>(() =>
+            json.Deserialize<Result>() ??
+            throw new JsonException($"Could not convert json element {json.ValueKind} to {typeof(Result).Name}"))
+        .Catch(err => new JsonError(err.Message, err));
+    
+    public static K<F, Result> deserialize<Result>(Stream stream)
+        => @try<Result>(() => JsonSerializer.Deserialize<Result>(stream) ??
+                              throw new JsonException(
+                                  $"Could not deserialize json stream result to {typeof(Result).Name}"))
+            .Catch(err => new JsonError(err.Message, err));
+    
+    public static K<F, Result> deserialize<Result>(string str)
+        => @try<Result>(() =>
+            JsonSerializer.Deserialize<Result>(str) ??
+            throw new JsonException($"Could not deserialize json result {limitLength(str)} to {typeof(Result).Name}"))
+        .Catch(err => new JsonError(err.Message, err));
+
+    
+    public static Func<JsonElement, K<F, JsonElement>> key(string objKey) => json => key(objKey, json);
+
+    public static K<F, JsonElement> key(string key, JsonElement json) =>
+        @try(() => json.GetProperty(key))
+            .Catch(err => new JsonError(keyErrorMessage(key, json), err));
+
+    private static string keyErrorMessage(string key, JsonElement json) =>
+        $"Unable to lookup key '{key}' in {json.ValueKind}: " + (json.ValueKind == JsonValueKind.Object
+            ? $"object contains keys: [{string.Join(", ", json.EnumerateObject().Select(kv => kv.Name))}]"
+            : shortToString(json));
+
+    public static Option<JsonElement> safeKey(string key, JsonElement json) =>
+        Try.lift(() => json.GetProperty(key)).ToOption();
+
+    public static K<F, Seq<JsonElement>> iterate(JsonElement json) =>
+        @try(json.EnumerateArray).Map(array => toSeq(array))
+            .Catch(err => new JsonError($"Unable to enumerate {json.ValueKind}: {shortToString(json)}"));
+
+    public static Func<JsonElement, K<F, JsonElement>> index(Index idx) => json => index(idx, json);
+    
+    public static K<F, JsonElement> index(Index idx, JsonElement json) =>
+        @try(() => json[idx.Value]).Catch(err => new JsonError(indexErrorMessage(idx, json), err));
+
+    private static string indexErrorMessage(Index idx, JsonElement json) =>
+        $"Unable to lookup index {idx.Value} in {json.ValueKind}: " + (json.ValueKind == JsonValueKind.Array
+            ? $"array is length {json.EnumerateArray().Count()}"
+            : shortToString(json));
+
+    private static string shortToString(JsonElement json) => limitLength(json.ToString());
+
+    private static string limitLength(string str) => 
+        str.Length < 100 ? str : str.Substring(0, 100) + "...";
+}
+public record JsonError(string Message, Option<Error> Inner = default) : Expected(Message, Code, Inner)
+{
+    public const int Code = 7654;
+}
+
+[Obsolete("Hopefully will be in main lib soon https://github.com/louthy/language-ext/pull/1539")]
+public static class Kleisli
+{
+    extension<M, A, B, C>(Func<A, K<M, B>> self) where M : Monad<M>
+    {
+        /// <summary>
+        /// Equivalent to Haskell's `>=>` or "fish" operator
+        /// </summary>
+        public static Func<A, K<M, C>> operator >> (Func<A, K<M, B>> bind1, Func<B, K<M, C>> bind2) =>
+            a => bind1(a).Bind(bind2);
+    }
+}
---
+++ LanguageExtJson.Tests/SystemTextSerializationTests.cs
@@ -0,0 +1,66 @@
+﻿using System.Text.Json;
+using LanguageExt;
+using static LanguageExt.Prelude;
+
+namespace LanguageExtJson.Tests;
+
+public class SystemTextSerializationTests
+{
+    private record TypesTest(int Int, Option<string> Opt, Seq<int> Seq);
+    [Fact]
+    public void SystemTextSerialize_WhenOptionOrSeqProperties_ShouldWork()
+    {
+        // Arrange
+        var @object = new TypesTest(1, "2", [3, 4]);
+        // Act
+        var result = SystemTextRoundTrip<TypesTest>(@object);
+        // Assert
+        Assert.Equal(@object, result);
+    }
+    
+    [Theory]
+    [InlineData(null)]
+    [InlineData("test")]
+    [InlineData("test123")]
+    public void SystemTextSerialize_WhenOption_ShouldWork(string? value)
+    {
+        // Arrange
+        var @object = Optional(value);
+        // Act
+        var result = SystemTextRoundTrip<Option<string>>(@object);
+        // Assert
+        Assert.Equal(@object, result);
+    }
+    
+    [Theory]
+    [InlineData()]
+    [InlineData(1, 2, 3)]
+    [InlineData(1, 2, 3, 4, 5, 6 ,7, 7, 8, 9, 0, 9, 8, 7, 6, 5, 4, 3, 2, 1)]
+    public void SystemTextSerializeWhenSeq_ShouldWork(params int[] values)
+    {
+        // Arrange
+        var @object = toSeq(values);
+        // Act
+        var result = SystemTextRoundTrip<Seq<int>>(@object);
+        // Assert
+        Assert.Equal(@object, result);
+    }
+    
+    [Fact]
+    public void SystemTextSerialize_WhenDeeplyNestedCustomTypes_ShouldWork()
+    {
+        // Arrange
+        var @object = Seq(
+            Some(Some(Seq(1, 2, 3))), 
+            Some<Option<Seq<int>>>(None),
+            Some(Some<Seq<int>>(Empty)),
+            None);
+        // Act
+        var result = SystemTextRoundTrip<Seq<Option<Option<Seq<int>>>>>(@object);
+        // Assert
+        Assert.Equal(@object, result);
+    }
+
+    private static T? SystemTextRoundTrip<T>(object @object) =>
+        JsonSerializer.Deserialize<T>(JsonSerializer.Serialize(@object));
+}
